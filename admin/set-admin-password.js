3db12cc908ceaa422c7389ba96f0873656400762
Aditya Gaddam <aditya.gaddam@okta.com>
2026-05-14 07:32:22 -0600

    restrict pending password file using existing lib

--- admin/set-admin-password.js
+++ admin/set-admin-password.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const execAsync = require('util').promisify(require('child_process').exec);
 const process = require('node:process');
+const filePermissions = require('../lib/filePermissions');
 
 /**
  * This script is used to set the password for the pending admin user.
@@ -24,14 +25,11 @@ if (!password) {
     const hash = await bcrypt.hash(password, 12);
     const filePath = path.join(__dirname, '.pending-admin-password');
     fs.writeFileSync(filePath, hash, 'utf8');
-    const commands = [
-      '$sddl = \'D:PAI(A;;FA;;;SY)(A;;FA;;;BA)(A;;0x1200a9;;;AC)(A;;0x1200a9;;;S-1-15-2-2)\'',
-      `$acl = Get-Acl -Path '${filePath}'`,
-      '$acl.SetSecurityDescriptorSddlForm($sddl)',
-      `Set-Acl -Path '${filePath}' -AclObject $acl`
-    ];
-    await execAsync('powershell -Command "' + commands.join(';') + '"');
-    process.stdout.write('\n');
+    await filePermissions.restrict({
+      filePath,
+      platform: process.platorm,
+      execAsync
+    });
   } catch (err) {
     process.stdout.write('Failed to save pending password: ' + err.message + '\n');
     process.exit(0);
